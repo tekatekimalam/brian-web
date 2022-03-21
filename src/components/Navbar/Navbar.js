da0e40d6ec12cb4fb1bb94406dd0266f28b5c5aa
alamasyarie <maiaelarsya@gmail.com>
2022-03-21 21:26:43 +0700

    Adding router

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,20 +1,43 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { FaBars, FaTimes } from "react-icons/fa";
 import { IconContext } from "react-icons/lib";
+import { Button } from "../../global.styled";
 import {
     MobileIcon,
     Nav,
     NavbarContainer,
+    NavBtnLink,
     NavIcon,
+    NavItem,
+    NavItemBtn,
+    NavLinks,
     NavLogo,
+    NavMenu,
 } from "./Navbar.styled";
 
 const Navbar = () => {
     const [clicked, setClicked] = useState(false);
+    const [button, setButton] = useState(true);
 
     const handleClick = () => {
         setClicked(!clicked);
     };
+    const closeMobileMenu = () => setClicked(false);
+
+    const showButton = () => {
+        if (window.innerWidth < 960) {
+            setButton(false);
+        } else {
+            setButton(true);
+        }
+    };
+
+    useEffect(() => {
+        showButton();
+    }, []);
+
+    window.addEventListener("resize", showButton);
+
     return (
         <>
             <IconContext.Provider value={{ color: "#fff" }}>
@@ -27,6 +50,34 @@ const Navbar = () => {
                         <MobileIcon onClick={handleClick}>
                             {clicked ? <FaTimes /> : <FaBars />}
                         </MobileIcon>
+                        <NavMenu clicked={clicked}>
+                            <NavItem onClick={handleClick}>
+                                <NavLinks to="/">Home</NavLinks>
+                            </NavItem>
+                            <NavItem onClick={handleClick}>
+                                <NavLinks to="/services">Services</NavLinks>
+                            </NavItem>
+                            <NavItem onClick={handleClick}>
+                                <NavLinks to="/products">Products</NavLinks>
+                            </NavItem>
+                            <NavItemBtn>
+                                {button ? (
+                                    <NavBtnLink to="/sign-up">
+                                        <Button primary>SIGN UP</Button>
+                                    </NavBtnLink>
+                                ) : (
+                                    <NavBtnLink to="/sign-up">
+                                        <Button
+                                            onClick={closeMobileMenu}
+                                            fontBig
+                                            primary
+                                        >
+                                            SIGN UP
+                                        </Button>
+                                    </NavBtnLink>
+                                )}
+                            </NavItemBtn>
+                        </NavMenu>
                     </NavbarContainer>
                 </Nav>
             </IconContext.Provider>
--- src/index.js
+++ src/index.js
@@ -10,4 +10,4 @@ ReactDOM.render(
     document.getElementById("root")
 );
 
-// 31:51
+// -1:44:49
