25e8fe0e080b5ee13517e889351675e8f5b6422b
alamasyarie <maiaelarsya@gmail.com>
2022-03-21 21:58:34 +0700

    Creating info section

---
+++ src/components/InfoSection/InfoSection.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { InfoSec } from "./InfoSection.styled";
+
+const InfoSection = ({ lightBg }) => {
+    return (
+        <>
+            <InfoSec></InfoSec>
+        </>
+    );
+};
+
+export default InfoSection;
